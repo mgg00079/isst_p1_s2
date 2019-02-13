0eb72a2370ee3bbc6bc004281a8c8ac831d27b63
Marta <Marta@DESKTOP-2H6P528>
2019-02-13 13:41:46 +0100

    Prueba código diap. 32

---
+++ isst_p1_s2/src/isst_p1_s2/Sesion2Parte1.java
@@ -0,0 +1,52 @@
+package isst_p1_s2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Sesion2Parte1
+ */
+@WebServlet("/Sesion2Parte1")
+public class Sesion2Parte1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession sesion = request.getSession(true);
+		
+		response.setContentType("text/html");
+		PrintWriter html = response.getWriter();
+		
+		html.print("<strong>");
+		
+		Date date = (Date)sesion.getAttribute("date");
+		
+		if(date != null) {
+			html.print("Último acceso de la sesión: " + date + "<br>");
+		}
+		
+		else {
+			html.print("Este es el primer acceso de la sesión <br>");
+		}
+		
+		date = new Date();
+		
+		sesion.setAttribute("date", date);
+		
+		html.print("Fecha actual: " + date);
+		html.print("</strong>");
+		
+	}
+
+}
