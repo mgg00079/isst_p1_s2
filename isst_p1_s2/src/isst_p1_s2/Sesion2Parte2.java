1cfc9028b1420e5b2a7e47e7333dd699b7a5c3a1
Marta <Marta@DESKTOP-2H6P528>
2019-02-24 20:30:37 +0100

    Últimas modificaciones.

--- isst_p1_s2/src/isst_p1_s2/Sesion2Parte2.java
+++ isst_p1_s2/src/isst_p1_s2/Sesion2Parte2.java
@@ -15,6 +15,8 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/Sesion2Parte2")
 public class Sesion2Parte2 extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -25,26 +27,23 @@ public class Sesion2Parte2 extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		//String url = "/isst_p1_s2/Formulario.html";
+		
+		HttpSession session = request.getSession(true);
+		
 		String nombre = request.getParameter("nombre");
 		String apellidos = request.getParameter("apellidos");
 		String email = request.getParameter("email");
 		
 		Usuario usuario = new Usuario(nombre, apellidos, email);
 		
-		HttpSession session = request.getSession(true);
 		session.setAttribute("usuario", usuario);
 		
-		Cookie c = new Cookie("emailCookie", email);
-		c.setMaxAge(10);
+		Cookie c = new Cookie("emailCookie", email); //Asignar el valor de email a la cookie emailCookie.
+		c.setMaxAge(60*60*60*365*2); //Tiempo de permanencia en el cliente. Para eliminar la cookie, poner el valor de este campo a 0.
 		c.setPath("/");
-		response.addCookie(c);
-		
-		//Para eliminar la cookie:
-		c.setMaxAge(0);
-		c.setPath("/");
-		response.addCookie(c);
+		response.addCookie(c); //Añadir cookie a la respuesta.
 		
 	}
-
 
 }
